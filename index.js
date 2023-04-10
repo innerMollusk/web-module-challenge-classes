20e00733c7eb32f2017fec1260be83d2ceb6d2ab
Ben Johnson <monk.sound.design@gmail.com>
2023-04-10 10:36:04 -0400

    tasks 1-3

--- index.js
+++ index.js
@@ -44,7 +44,22 @@ class Airplane {
 */
 
 class Person {
-  
+  constructor(name, age) {
+   this.name = name;
+   this.age = age;
+   this.stomach = [];
+  }
+  eat(edible) {
+    if (this.stomach.length < 10) {
+      this.stomach.push(edible);
+    }
+  }
+  poop() {
+    this.stomach = [];
+  }
+  toString() {
+    return `${this.name}, ${this.age}`;
+  }
 }
 
 /*
@@ -62,7 +77,26 @@ class Person {
 */
 
 class Car {
-  
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons) {
+    this.tank = this.tank + gallons;
+  }
+  drive(distance) {
+    const driveMiles = this.tank * this.milesPerGallon;
+    if(distance < driveMiles) {
+      this.odometer = this.odometer + distance;
+      this.tank = this.tank - (distance / this.milesPerGallon);
+    } else {
+      this.odometer = this.odometer + driveMiles;
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer} miles!`;
+    }
+  }
 }
 
 /*
@@ -79,7 +113,14 @@ class Car {
 */
 
 class Lambdasian {
-  
+  constructor({ name, age, location }) {
+    this.name = name;
+    this.age = age;
+    this.location = location;
+  }
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
 }
 
 /*
